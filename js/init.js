9b171220553bf0c8a2645e4e3e58f09c17fe323c
smurdok <sirmurdok@gmail.com>
2015-03-18 21:39:37 -0600

    Iniciando

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('select').material_select();
+    $('.dropdown-button').dropdown({hover: false});
+    // $('.scrollspy').scrollSpy();
+    // $('.button-collapse').sideNav({'edge': 'left'});
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
